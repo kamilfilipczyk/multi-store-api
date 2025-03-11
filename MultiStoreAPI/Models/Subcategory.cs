9e676700085b6e78e5d148ba5a244783bdd1ca45
kamilfilipczyk <kamil.filipczyk.kontakt@gmail.com>
2025-03-11 15:41:30 +0100

    Add basic Subcategory model

---
+++ MultiStoreAPI/Models/Subcategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiStoreAPI.Models
+{
+    public class Subcategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(60)]
+        public required string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+        //relations
+        public Category? Category { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
