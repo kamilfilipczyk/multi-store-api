acb22ea9d131a8d7483d370eaef57fda1d2bf102
kamilfilipczyk <kamil.filipczyk.kontakt@gmail.com>
2025-03-11 15:41:10 +0100

    Add basic Product model

---
+++ MultiStoreAPI/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiStoreAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(5)]
+        [MaxLength(150)]
+        public required string Name { get; set; }
+
+        public required decimal Price { get; set; }
+
+        public int StockValue { get; set; }
+
+        [MinLength(5)]
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        public float Rating { get; set; } = 0;
+
+        public int SubcategoryId { get; set; }
+
+
+        //relations
+        public Subcategory? Subcategory { get; set; }
+    }
+}
