4b3b3ed6cd3e73a202f312d086e18a5f906f0172
kamilfilipczyk <kamil.filipczyk.kontakt@gmail.com>
2025-03-11 15:41:20 +0100

    Add basic Category model

---
+++ MultiStoreAPI/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiStoreAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(60)]
+        public required string Name { get; set; }
+
+
+        //relations
+        public ICollection<Subcategory>? Subcategories { get; set; }
+    }
+}
