a50991fb67125dae84125a68c6543b594e613bed
kamilfilipczyk <kamil.filipczyk.kontakt@gmail.com>
2025-03-12 12:16:54 +0100

    Add DbContext class

---
+++ MultiStoreAPI/Data/MultiStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiStoreAPI.Models;
+
+namespace MultiStoreAPI.Data
+{
+    public class MultiStoreDbContext : DbContext
+    {
+        public MultiStoreDbContext(DbContextOptions<MultiStoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Subcategory> Subcategories { get; set; }
+    }
+}
